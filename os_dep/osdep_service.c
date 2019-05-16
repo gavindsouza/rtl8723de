5e82ae6d80c0728bada3b484face63ecddf73e28
Gavin Dsouza <gavin18d@gmail.com>
2019-05-16 22:39:12 +0530

    [FIXES] kernel for 5.1

--- os_dep/osdep_service.c
+++ os_dep/osdep_service.c
@@ -2085,7 +2085,7 @@ static int isFileReadable(const char *path, u32 *sz)
 		ret = PTR_ERR(fp);
 	else {
 		oldfs = get_fs();
-		set_fs(get_ds());
+		set_fs(KERNEL_DS);
 
 		if (1 != readFile(fp, &buf, 1))
 			ret = PTR_ERR(fp);
@@ -2123,7 +2123,15 @@ static int retriveFromFile(const char *path, u8 *buf, u32 sz)
 			RTW_INFO("%s openFile path:%s fp=%p\n", __FUNCTION__, path , fp);
 
 			oldfs = get_fs();
-			set_fs(get_ds());
+			/*
+			* pre 5.1.x : set_fs(get_ds())
+			* get_ds() returned address limits for process
+			*
+			* post 5.1.x: set_fs(KERNEL_DS)
+			* KERNEL_DS => kernel data segment
+			* other option would be USER_DS but the former fits the purpose?
+			*/
+			set_fs(KERNEL_DS);
 			ret = readFile(fp, buf, sz);
 			set_fs(oldfs);
 			closeFile(fp);
@@ -2158,7 +2166,7 @@ static int storeToFile(const char *path, u8 *buf, u32 sz)
 			RTW_INFO("%s openFile path:%s fp=%p\n", __FUNCTION__, path , fp);
 
 			oldfs = get_fs();
-			set_fs(get_ds());
+			set_fs(KERNEL_DS);
 			ret = writeFile(fp, buf, sz);
 			set_fs(oldfs);
 			closeFile(fp);
